47cf947a03d35d552b758462e263db39f14edffe
Leandro <leandroquiroga9514@gmail.com>
2022-01-22 18:25:20 -0300

    Init App

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { TodoApp } from './TodoApp';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TodoApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
